15f198a4fbcbc7ce535d24faf2a5e146d1f33dfc
Michael Specht <micha.specht@gmail.com>
2019-02-06 20:01:09 +0100

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -17,7 +17,9 @@ class Main < Sinatra::Base
             ws = Faye::WebSocket.new(request.env)
             
             ws.on(:open) do |event|
-                puts 'On Open'
+            end
+
+            ws.on(:close) do |event|
             end
 
             ws.on(:message) do |msg|
@@ -25,66 +27,55 @@ class Main < Sinatra::Base
                 request = JSON.parse(msg.data)
                 STDERR.puts request.to_json
                 if request['action'] == 'open'
-                    @@clients[client_id] = TCPSocket.new(request['host'], request['port'])
-                    @@clients[client_id].set_encoding('UTF-8')
-                    if request['tls']
-                        ssl_context = OpenSSL::SSL::SSLContext.new()
-#                         ssl_context.ssl_version = :SSLv23
-                        @@clients[client_id] = OpenSSL::SSL::SSLSocket.new(@@clients[client_id], ssl_context)
-                        @@clients[client_id].sync_close = true
-                        @@clients[client_id].connect
-                        @@clients[client_id].io.set_encoding('UTF-8')
+                    begin
+                        @@clients[client_id] = TCPSocket.new(request['host'], request['port'])
+                        @@clients[client_id].set_encoding('UTF-8')
+                        if request['tls']
+                            ssl_context = OpenSSL::SSL::SSLContext.new()
+    #                         ssl_context.ssl_version = :SSLv23
+                            @@clients[client_id] = OpenSSL::SSL::SSLSocket.new(@@clients[client_id], ssl_context)
+                            @@clients[client_id].sync_close = true
+                            @@clients[client_id].connect
+                            @@clients[client_id].io.set_encoding('UTF-8')
+                        end
+                    rescue StandardError => e
+                        @@clients.delete(client_id)
+                        ws.send({:error => e.to_s}.to_json)
                     end
-#                     ws.send('OPENED')
-                    Thread.new do 
-#                         while true do
-#                             buffer = nil
-#                             begin
-#                                 buffer = @@clients[client_id].read_nonblock(1024)
-#                             rescue EOFError
-#                                 STDERR.puts "EOFError"
-#                                 break
-#                             rescue OpenSSL::SSL::SSLErrorWaitReadable
-#                                 STDERR.puts "SSLErrorWaitReadable"
-#                                 sleep(1)
-#                             end 
-#                             if buffer
-#                                 ws.send(buffer)
-#                             end
-#                             if @@clients[client_id].closed?
-#                                 break
-#                             end
-#                         end
-#                         STDERR.puts "socket closed"
-                        while true do
-                            break if @@clients[client_id].closed? || @@clients[client_id].eof?
-                            STDERR.puts "select >"
-                            s = IO.select([@@clients[client_id]])
-                            STDERR.puts "<"
+                    if @@clients[client_id]
+                        ws.send({:connected => true}.to_json)
+                        Thread.new do 
                             while true do
-                                buffer = nil
-                                begin
-                                    buffer = @@clients[client_id].read_nonblock(4096)
-                                    buffer.force_encoding('UTF-8')
-                                rescue EOFError
-                                    STDERR.puts "EOFError"
-                                    break
-                                rescue OpenSSL::SSL::SSLErrorWaitReadable
-                                    STDERR.puts "SSLErrorWaitReadable"
-                                    break
-                                rescue IO::EAGAINWaitReadable
-                                    STDERR.puts "EAGAINWaitReadable"
-                                    break
-                                end
-                                if buffer
-                                    STDERR.puts "Received #{buffer.size} bytes."
-                                    ws.send(buffer)
+                                break if @@clients[client_id].closed? || @@clients[client_id].eof?
+                                STDERR.puts "select >"
+                                s = IO.select([@@clients[client_id]])
+                                STDERR.puts "<"
+                                while true do
+                                    buffer = nil
+                                    begin
+                                        buffer = @@clients[client_id].read_nonblock(4096)
+                                        buffer.force_encoding('UTF-8')
+                                    rescue EOFError
+                                        STDERR.puts "EOFError"
+                                        break
+                                    rescue OpenSSL::SSL::SSLErrorWaitReadable
+                                        STDERR.puts "SSLErrorWaitReadable"
+                                        break
+                                    rescue IO::EAGAINWaitReadable
+                                        STDERR.puts "EAGAINWaitReadable"
+                                        break
+                                    end
+                                    if buffer
+                                        STDERR.puts "Received #{buffer.size} bytes."
+                                        ws.send(buffer)
+                                    end
+                                    break if @@clients[client_id].closed?
                                 end
                                 break if @@clients[client_id].closed?
                             end
-                            break if @@clients[client_id].closed?
+                            STDERR.puts "socket closed"
+                            ws.send({:connected => false}.to_json)
                         end
-                        STDERR.puts "socket closed"
                     end
                 elsif request['action'] == 'send'
                     if @@clients[client_id]
@@ -104,10 +95,6 @@ class Main < Sinatra::Base
                         ws.send('CLOSE')
                     end
                 end
-            end
-
-            ws.on(:close) do |event|
-                puts 'On Close'
             end
 
             ws.rack_response
