9d07f8e85091249e7b7fd609bc8ada23c518dd6d
Michael Specht <micha.specht@gmail.com>
2019-02-07 12:46:52 +0100

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -24,73 +24,77 @@ class Main < Sinatra::Base
 
             ws.on(:message) do |msg|
                 client_id = request.env['HTTP_SEC_WEBSOCKET_KEY']
-                request = JSON.parse(msg.data)
-                STDERR.puts request.to_json
-                if request['action'] == 'open'
-                    begin
-                        @@clients[client_id] = TCPSocket.new(request['host'], request['port'])
-                        @@clients[client_id].set_encoding('UTF-8')
-                        if request['tls']
-                            ssl_context = OpenSSL::SSL::SSLContext.new()
-    #                         ssl_context.ssl_version = :SSLv23
-                            @@clients[client_id] = OpenSSL::SSL::SSLSocket.new(@@clients[client_id], ssl_context)
-                            @@clients[client_id].sync_close = true
-                            @@clients[client_id].connect
-                            @@clients[client_id].io.set_encoding('UTF-8')
+                begin
+                    request = JSON.parse(msg.data)
+                    STDERR.puts request.to_json
+                    if request['action'] == 'open'
+                        begin
+                            @@clients[client_id] = TCPSocket.new(request['host'], request['port'])
+                            @@clients[client_id].set_encoding('UTF-8')
+                            if request['tls']
+                                ssl_context = OpenSSL::SSL::SSLContext.new()
+#                                 ssl_context.ssl_version = :SSLv23
+                                @@clients[client_id] = OpenSSL::SSL::SSLSocket.new(@@clients[client_id], ssl_context)
+                                @@clients[client_id].sync_close = true
+                                @@clients[client_id].connect
+                                @@clients[client_id].io.set_encoding('UTF-8')
+                            end
+                        rescue StandardError => e
+                            @@clients.delete(client_id)
+                            ws.send({:connection_error => e.to_s, :host => request['host'], :port => request['port'], :tls => request['tls']}.to_json)
                         end
-                    rescue StandardError => e
-                        @@clients.delete(client_id)
-                        ws.send({:error => e.to_s}.to_json)
-                    end
-                    if @@clients[client_id]
-                        ws.send({:connected => true, :host => request['host'], :port => request['port'], :address => @@clients[client_id].peeraddr(:numeric)[3]}.to_json)
-                        STDERR.puts @@clients[client_id].inspect
-                        Thread.new do 
-                            while true do
-                                break if @@clients[client_id].closed? || @@clients[client_id].eof?
-                                STDERR.puts "select >"
-                                s = IO.select([@@clients[client_id]])
-                                STDERR.puts "<"
+                        if @@clients[client_id]
+                            ws.send({:connected => true, :host => request['host'], :port => request['port'], :tls => request['tls'], :address => @@clients[client_id].peeraddr[3]}.to_json)
+                            STDERR.puts @@clients[client_id].inspect
+                            Thread.new do 
                                 while true do
-                                    buffer = nil
-                                    begin
-                                        buffer = @@clients[client_id].read_nonblock(4096)
-                                        buffer.force_encoding(Encoding::UTF_8)
-                                        buffer.encode!(Encoding::UTF_16LE, invalid: :replace, replace: "\uFFFD")
-                                        buffer.encode!(Encoding::UTF_8)
-                                    rescue EOFError
-                                        STDERR.puts "EOFError"
-                                        break
-                                    rescue OpenSSL::SSL::SSLErrorWaitReadable
-                                        STDERR.puts "SSLErrorWaitReadable"
-                                        break
-                                    rescue IO::EAGAINWaitReadable
-                                        STDERR.puts "EAGAINWaitReadable"
-                                        break
-                                    end
-                                    if buffer
-                                        STDERR.puts "Received #{buffer.size} bytes."
-                                        ws.send({:message => buffer}.to_json)
+                                    break if @@clients[client_id].closed? || @@clients[client_id].eof?
+                                    STDERR.puts "select >"
+                                    s = IO.select([@@clients[client_id]])
+                                    STDERR.puts "<"
+                                    while true do
+                                        buffer = nil
+                                        begin
+                                            buffer = @@clients[client_id].read_nonblock(4096)
+                                            buffer.force_encoding(Encoding::UTF_8)
+                                            buffer.encode!(Encoding::UTF_16LE, invalid: :replace, replace: "\uFFFD")
+                                            buffer.encode!(Encoding::UTF_8)
+                                        rescue EOFError
+                                            STDERR.puts "EOFError"
+                                            break
+                                        rescue OpenSSL::SSL::SSLErrorWaitReadable
+                                            STDERR.puts "SSLErrorWaitReadable"
+                                            break
+                                        rescue IO::EAGAINWaitReadable
+                                            STDERR.puts "EAGAINWaitReadable"
+                                            break
+                                        end
+                                        if buffer
+                                            STDERR.puts "Received #{buffer.size} bytes."
+                                            ws.send({:message => buffer}.to_json)
+                                        end
+                                        break if @@clients[client_id].closed?
                                     end
                                     break if @@clients[client_id].closed?
                                 end
-                                break if @@clients[client_id].closed?
+                                STDERR.puts "socket closed"
+                                ws.send({:connected => false}.to_json)
                             end
-                            STDERR.puts "socket closed"
-                            ws.send({:connected => false}.to_json)
                         end
+                    elsif request['action'] == 'send'
+                        if @@clients[client_id]
+                            @@clients[client_id].write(request['message'].strip)
+                            @@clients[client_id].write("\r\n")
+                        end
+                    elsif request['action'] == 'close'
+                        begin
+                            @@clients[client_id].close
+                        rescue
+                        end
+                        ws.send({:connected => false}.to_json)
                     end
-                elsif request['action'] == 'send'
-                    if @@clients[client_id]
-                        @@clients[client_id].write(request['message'].strip)
-                        @@clients[client_id].write("\r\n")
-                    end
-                elsif request['action'] == 'close'
-                    begin
-                        @@clients[client_id].close
-                    rescue
-                    end
-                    ws.send({:connected => false}.to_json)
+                rescue
+                    STDERR.puts "um..."
                 end
             end
 
